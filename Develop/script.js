ffe2653d2f325d760312056dcf7a2a723a883511
Nick <nicksweetj@gmail.com>
2023-05-14 21:35:43 -0400

    added more html for more time slots

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,21 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+  var saveButtonEl = $(".saveBtn");
+  var description = $(".description")
+  var item = "17";
+  saveButtonEl.on("click", function () {
+    localStorage.setItem("data", item);
+  })
+
+
+  var now = dayjs().format('MM/DD/YYYY');
+  $("#currentDay").text(now);
+});
+
+
+
+ // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -20,7 +34,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-  var now = dayjs().format('MM/DD/YYYY');
-  var date = $("#currentDay");
-  date.text(now);
-});
